1beb972893907d53eb5ad131b679389dd424e3f7
mathias8dev <cd6561@engie.com>
2024-10-02 21:02:12 +0200

    feature: Update of the version from 1.0.1 to 1.0.2

--- yup-compose/build.gradle.kts
+++ yup-compose/build.gradle.kts
@@ -72,7 +72,7 @@ configure<PublishingExtension> {
         register<MavenPublication>("release") {
             groupId = "io.github.mathias8dev"
             artifactId = "yup-compose"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
